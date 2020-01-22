2783fd203e04624daaf180498e359618e0913d63
Scott Taylor <scottntaylor21@gmail.com>
2020-01-21 19:00:23 -0800

    Routes Started

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,30 @@
+// const router = require("express").Router();
+const db = require("./Develop/models");
+
+module.exports = function(app) {
+    app.get("/api/workouts", function(req, res){
+        //MongoDB to show workouts
+        db.Workout.find({})
+        .then(dbWorkout => {
+          res.json(dbWorkout);
+        })
+        .catch(err => {
+          res.json(err);
+        });
+    });
+
+    app.get("/api/workouts/:id", function(req, res){
+        //MongoDB to show specific workout
+        db.Workout.findById(id)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+    });
+
+    app.post("/api/workouts", function(req, res){
+        //MongoDB to create new workout
+    });
+}
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./Develop/models");
+
 const app = express();
 
 app.use(logger("dev"));
@@ -15,6 +15,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/userdb", { useNewUrlParser: true });
+
+app.use(require("./routes/apiRoutes.js"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
