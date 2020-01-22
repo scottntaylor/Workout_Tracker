625d2188def9d4823834252b3710e1cadf0d8637
Scott Taylor <scottntaylor21@gmail.com>
2020-01-21 19:22:35 -0800

    Routes continued

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,30 +1,38 @@
 // const router = require("express").Router();
 const db = require("./Develop/models");
 
-module.exports = function(app) {
-    app.get("/api/workouts", function(req, res){
+module.exports = function (app) {
+    app.get("/api/workouts", function (req, res) {
         //MongoDB to show workouts
         db.Workout.find({})
-        .then(dbWorkout => {
-          res.json(dbWorkout);
-        })
-        .catch(err => {
-          res.json(err);
-        });
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
 
-    app.get("/api/workouts/:id", function(req, res){
+    app.get("/api/workouts/:id", function (req, res) {
         //MongoDB to show specific workout
         db.Workout.findById(id)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
 
-    app.post("/api/workouts", function(req, res){
+    app.post("/api/workouts", function (req, res) {
         //MongoDB to create new workout
+        console.log(req.body);
+        db.Workout.insert(req.body)
+            .then(newWorkout => {
+                res.json(newWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
     });
 }
